75978f3aaaad8d73fe43c83940d5dd4b93f00e20
Kripesh <kg9773092+github@gmail.com>
2026-01-16 09:19:33 +0545

    Websocket ping pong game

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,11 +4,10 @@ const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", function (socket) {
   console.log("User connected");
-  setInterval(function () {
-    socket.send("Current price of solana is: " + Math.random() * 100);
-  }, 500);
 
   socket.on("message", (e) => {
-    console.log(e.toString());
+    if (e.toString() === "ping") {
+      socket.send("pong");
+    }
   });
 });
